d52c2a1dd310b5c54ac3a5d86564c26b67acb8a9
Sinkvane <69595744+sinkvane@users.noreply.github.com>
2022-11-14 02:06:27 +0700

    validate js added

--- markup/static/js/plugins/main.js
+++ markup/static/js/plugins/main.js
@@ -1,10 +1,10 @@
 // Lazy Load изображений
 
-if (document.querySelector(".lazy")) {
-    let lazyLoadInstance = new LazyLoad({
-        elements_selector: ".lazy"
-    });
-}
+// if (document.querySelector(".lazy")) {
+//     let lazyLoadInstance = new LazyLoad({
+//         elements_selector: ".lazy"
+//     });
+// }
 
 // Инициализация свайпера
 
@@ -27,3 +27,34 @@ if (document.querySelector(".lazy")) {
 //         prevEl: ".index-button-prev",
 //     },
 // });
+
+// Маска для телефона
+// let inputs = document.querySelectorAll('input[type="tel"]');
+// let im = new Inputmask('+7 (999) 999-99-99');
+// im.mask(inputs);
+
+
+// Валидация различных полей ввода
+
+// function validateForms(selector, rules) {
+//     new window.JustValidate(selector, {
+//         rules: rules,
+//         submitHandler: function (form, values, ajax) {
+//             console.log(form);
+
+//             let formData = new FormData(form);
+
+//             fetch("send.php", {
+//                 method: "POST",
+//                 body: formData
+//             })
+//                 .then(function(data) {
+//                     console.log(data);
+//                     console.log('Отправлено');
+//                     form.reset();
+//                 });
+//         }
+//     });
+// }
+
+// validateForms('.form', { email: { required: true, email: true }, fio: { required: true }, tel: { required: true } });
