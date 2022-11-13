5640ab5b9c5a436cf358ee6853acaddeaac3b34a
Sinkvane <69595744+sinkvane@users.noreply.github.com>
2022-11-14 02:41:11 +0700

    final fixes

--- markup/static/js/plugins/main.js
+++ markup/static/js/plugins/main.js
@@ -58,3 +58,34 @@
 // }
 
 // validateForms('.form', { email: { required: true, email: true }, fio: { required: true }, tel: { required: true } });
+
+
+// Плавный скролл на странице
+
+SmoothScroll({
+    // Время скролла 400 = 0.4 секунды
+    animationTime: 1000,
+    // Размер шага в пикселях
+    stepSize: 100,
+
+    // Дополнительные настройки:
+
+    // Ускорение
+    accelerationDelta: 30,
+    // Максимальное ускорение
+    accelerationMax: 2,
+
+    // Поддержка клавиатуры
+    keyboardSupport: true,
+    // Шаг скролла стрелками на клавиатуре в пикселях
+    arrowScroll: 50,
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+
+    // Поддержка тачпада
+    touchpadSupport: true,
+})
